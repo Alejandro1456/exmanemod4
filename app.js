5c43789b76e9fe45d7c7269714ed2b600118f8e6
Alejandro1456 <alejoruben150@gmail.com>
2023-09-16 13:17:58 -0400

    examen mod 4

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+//ruta public
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+dotenv.config({path: './env/.env'})
+
+app.use(cookieParser())
+
+app.use('/', require('./routes/router'))
+
+app.use(function(req, res, next){
+    if (!req.user)
+    res.header('Cache-Control', 'private, no-cache. no-store, must-revalidate');
+next();
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Todo posi usa esto http://localhost:3000')
+})
